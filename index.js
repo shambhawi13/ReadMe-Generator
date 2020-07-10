57ebe43262534fe5fd208ff3d47852fd2d17dbfd
Sham <shambhawi.kumari1393@gmail.com>
2020-07-09 20:01:59 -0700

    added prompt logic

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
 let fs = require('fs');
 let inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "Provide github username"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Describe your project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Explain process of installation"
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "How can your application be used?"
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "Enter license detail, If any?"
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "List all contributors"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Enter test detail."
+    }
+]).then(function (data) {
+
+    fs.writeFile("README.md", JSON.stringify(data, null, '\t'), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
+
+});

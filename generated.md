e9e96f7e3d40e0a0dc2f73c9fd384ecfcf355ea8
Sham <shambhawi.kumari1393@gmail.com>
2020-07-10 20:53:12 -0700

    added conditional template

---
+++ generated.md
@@ -0,0 +1,33 @@
+# dhf
+
+
+
+
+## Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+* [Contributing](#contributing)
+* [Tests](#Tests)
+
+## Installation
+jasdj
+
+## Usage
+ksdj
+
+## Credits
+sds
+
+
+
+
+
+
+
+
+
+## Tests
+sdd
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ inquirer.prompt([
     },
     {
         type: "input",
-        name: "Description",
+        name: "description",
         message: "Describe your project?"
     },
     {
@@ -24,27 +24,69 @@ inquirer.prompt([
     },
     {
         type: "input",
-        name: "Usage",
+        name: "usage",
         message: "How can your application be used?"
     },
     {
         type: "input",
-        name: "License",
+        name: "license",
         message: "Enter license detail, If any?"
     },
     {
         type: "input",
-        name: "Contributing",
+        name: "credit",
         message: "List all contributors"
     },
     {
         type: "input",
+        name: "contributing",
+        message: "Describe steps how developer can contribute to your project."
+    },
+    {
+        type: "input",
         name: "test",
-        message: "Enter test detail."
+        message: "Enter any test you have written."
     }
 ]).then(function (data) {
 
-    fs.writeFile("README.md", JSON.stringify(data, null, '\t'), function (err) {
+    console.log(data);
+let template = 
+`# ${data.title}
+
+${data.description?'## Description' : ''}
+${data.description}
+
+## Table of Contents 
+${data.installation?'* [Installation](#installation)' : ''}
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+* [Contributing](#contributing)
+* [Tests](#Tests)
+
+${data.installation?'## Installation' : ''}
+${data.installation}
+
+${data.usage?'## Usage' : ''}
+${data.usage}
+
+${data.credit?'## Credits' : ''}
+${data.credit}
+
+${data.license?'## License' : ''}
+${data.license}
+
+${data.badges?'## Badges' : ''}
+
+${data.contributing?'## Contributing' : ''}
+${data.contributing}
+
+${data.test?'## Tests' : ''}
+${data.test}`;
+
+    //JSON.stringify(data, null, '\t')
+    fs.writeFile("generated.md", template , data, function (err) {
 
         if (err) {
             return console.log(err);
